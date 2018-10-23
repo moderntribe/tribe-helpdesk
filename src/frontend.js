55b8f64d99dfdab61d57437e4e7d6e9c2cabc441
barryhughes <barry@codingkills.me>
2018-10-23 07:15:28 -0700

    Adjust verbiage, avoid usage of 'let' for max compat

--- src/frontend.js
+++ src/frontend.js
@@ -8,8 +8,10 @@
 	var loggedOutUsersAdvice = 
 		  '<p class="tribe-support-advice"> <strong>Existing customer? To receive the fastest support possible, '
 		+ 'please <a href="https://support.theeventscalendar.com/login">login</a> before posting.</strong> '
-		+ 'In some cases, you may need to perform a password reset first of all: to learn more about accessing '
-		+ 'support, or if you experience any problems, please refer to '
+		+ 'In some cases, you may need to register first of all: be sure to do so using the same email address '
+		+ 'you used when purchasing your license key!</p>'
+		+ '<p class="tribe-support-advice">To learn more about accessing support or if you experience any problems, '
+		+ 'please refer to '
 		+ '<a href="https://support.theeventscalendar.com/812859-Accessing-Support">this article</a>.</p>'
 
 	jQuery( function( $ ) { 
@@ -42,8 +44,8 @@
 		 * ("http://" or "https://") stripped for safer comparisons.
 		 */
 		function getCurrentUrl() {
-			let url = document.location.toString()
-			let protocolSeparator = url.match( /:\/\// )
+			var url = document.location.toString()
+			var protocolSeparator = url.match( /:\/\// )
 
 			// Remove trailing slash for uniformity
 			if ( '/' === url.slice( -1 ) ) {
