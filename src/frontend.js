db4be1dd339fb1ec4a40f81f0403f5a05401ea28
barryhughes <barry@codingkills.me>
2018-12-27 14:59:47 -0800

    Change alert message

--- src/frontend.js
+++ src/frontend.js
@@ -7,10 +7,11 @@
 	// Marteking alert
 	var marketingAlert =
 		  '<div id="tribe-marketing-alert">'
-		+ '<div> <span class="emoji">🆕</span> </div>'
-		+ '<div> Hey friends! We\'re nearing the holiday season and support requests are higher than normal. We\'re '
-		+ 'chugging along to reply to as many folks as we can, but in the meantime, check out how Event Tickets Plus '
-		+ 'now has a better way of collecting attendee details. '
+		+ '<div> <span class="emoji">🛈</span> </div>'
+		+ '<div> Our team are starting to come back online after some well earned rest and recuperation over the festive period. However, due to increased '
+		+ 'demand for support services, there may be a slightly longer wait than is usual before we can reply to your next ticket: thank you for your patience '
+		+ 'in the meantime! <br/><br/>'
+		+ 'If you didn&#146;t catch the news already, please also note that Event Tickets Plus now has a better way of collecting attendee details. '
 		+ '<a target="_blank" href="https://theeventscalendar.com/a-new-and-better-way-to-collect-attendee-details-with-event-tickets-plus/">Learn more</a>.'
 		+ '</div></div>'
 	
