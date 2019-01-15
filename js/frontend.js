1091d2bd4c095cdc781cd781bef11328fd0e4fa3
geoffgraham <geoff@geoffgraham.me>
2019-01-15 12:32:52 -0800

    Updates response time from 2 to 3 days.
    Signed-off-by: geoffgraham <geoff@geoffgraham.me>

--- js/frontend.js
+++ js/frontend.js
@@ -8,7 +8,7 @@
 	var marketingAlert =
 		  '<div id="tribe-marketing-alert">'
 		+ '<div><span class="emoji">⚠️</span></div>'
-		+ '<div><strong>Heads up!</strong> Response times for support requests are a little longer than usual. Please expect up to 2 business days for a reply.'
+		+ '<div><strong>Heads up!</strong> Response times for support requests are a little longer than usual. Please expect up to 3 business days for a reply.'
 		+ '</div></div>'
 	
 	// Some general advice we'll show as needed in the jumbotron area
