ce08bb0be00992a3804ea1649896cc5d7e669dff
geoffgraham <geoff@geoffgraham.me>
2019-01-15 14:22:14 -0800

    Splits JS into concatenated files.
    Signed-off-by: geoffgraham <geoff@geoffgraham.me>

---
+++ js/marketingAlert.js
@@ -0,0 +1,10 @@
+// Marketing alert
+var marketingAlert =
+'<div id="tribe-marketing-alert">'
++ '<div> <span class="emoji">🛈</span> </div>'
++ '<div> Our team are starting to come back online after some well earned rest and recuperation over the festive period. However, due to increased '
++ 'demand for support services, there may be a slightly longer wait than is usual before we can reply to your next ticket: thank you for your patience '
++ 'in the meantime! <br/><br/>'
++ 'If you didn&#146;t catch the news already, please also note that Event Tickets Plus now has a better way of collecting attendee details. '
++ '<a target="_blank" href="https://theeventscalendar.com/a-new-and-better-way-to-collect-attendee-details-with-event-tickets-plus/">Learn more</a>.'
++ '</div></div>'
