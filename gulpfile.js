7f29d0cf830e14b181552ced611e484970422ff5
barryhughes <barry@codingkills.me>
2019-01-17 15:55:25 -0800

    Logic to remove empty category boxes | #120916

--- gulpfile.js
+++ gulpfile.js
@@ -4,53 +4,46 @@ var sass            = require('gulp-ruby-sass');
 var autoprefixer    = require('gulp-autoprefixer');
 var cleanCSS        = require('gulp-clean-css');
 var rename          = require('gulp-rename');
-var header          = require('gulp-header');
-var footer          = require('gulp-footer');
 var concat          = require('gulp-concat');
 var uglify          = require('gulp-uglify');
 var path            = require('path');
 var inject          = require('gulp-inject');
 var livereload      = require('gulp-livereload');
-var fs              = require('fs');
 
 /**
  * Compile styles
  */
 gulp.task('styles', function() {
-	return sass( 'scss/*' )
-	.pipe(autoprefixer())
-	.pipe(cleanCSS())
-	.pipe(rename('style.min.css'))
-	.pipe(gulp.dest('dist'))
-	.pipe(livereload());
+  return sass( 'scss/*' )
+  .pipe(autoprefixer())
+  .pipe(cleanCSS())
+  .pipe(rename('style.min.css'))
+  .pipe(gulp.dest('dist'))
+  .pipe(livereload());
 });
 
 /**
  * Concatenate scripts
  */
 gulp.task('scripts', function(){
-	return gulp.src([
-		'js/jQueryCheck.js',
-		'js/marketingAlert.js',
-		'js/loggedOutUserAdvice.js',
-		'js/loginFormAdvice.js',
-		'js/loggedInLanderPageAdvice.js',
-		'js/injectContent.js'
-	])
-	.pipe(concat('scripts.js'))
-	.pipe(uglify())
-	.pipe(header(fs.readFileSync('js/header.txt', 'utf8')))
-	.pipe(footer(fs.readFileSync('js/footer.txt', 'utf8')))
-	.pipe(rename('scripts.min.js'))
-	.pipe(gulp.dest('dist'))
+  return gulp.src('./js/*.js')
+  .pipe(concat('frontend.js'))
+  .pipe(uglify())
+  .pipe(rename('frontend.min.js'))
+  .pipe(gulp.dest('dist'))
 });
 
 /**
  * Watch task
  */
 gulp.task('watch', function(){
-	livereload.listen();
-	gulp.watch('scss/**/*.scss', ['styles']);
+  livereload.listen();
+
+  gulp.watch('scss/*', ['styles']);
+  gulp.watch('js/*.js', ['scripts'] ).on('change',function(file) {
+    livereload.changed(file.path);
+  });
+
 });
 
 /**
