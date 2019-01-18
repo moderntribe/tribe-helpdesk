34d0e0d2df36104175aad35bb9a2f34e0efa9685
geoffgraham <geoff@geoffgraham.me>
2019-01-18 11:57:47 -0800

    Revert "Splits JS into concatenated files."
    This reverts commit e91787156d579dc9f834cb9c22cbe3c12ea3ffda.

--- gulpfile.js
+++ gulpfile.js
@@ -26,15 +26,10 @@ gulp.task('styles', function() {
  * Concatenate scripts
  */
 gulp.task('scripts', function(){
-  return gulp.src([
-    'js/jqueryCheck.js',
-    'js/marketingAlert.js',
-    'js/loginFormAdvice.js',
-    'js/injectContent.js'
-  ])
+  return gulp.src('js/frontend.js')
   .pipe(concat('frontend.js'))
   .pipe(uglify())
-  .pipe(rename('scripts.min.js'))
+  .pipe(rename('frontend.min.js'))
   .pipe(gulp.dest('dist'))
 });
 
