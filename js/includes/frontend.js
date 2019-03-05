e18801b89a118e5b9fcdd8e8950017ccf81492f7
barryhughes <barry@codingkills.me>
2019-03-05 13:49:49 -0800

    Add new directories (globals and includes) | spotfix

---
+++ js/includes/frontend.js
@@ -0,0 +1,90 @@
+// Marketing alert
+var marketingAlert = `
+	<div id="tribe-marketing-alert">
+		<div><span class="emoji">⚠️</span></div>
+		<div>
+			<strong>Heads up!</strong> 
+			Response times for support requests are a little longer than usual. 
+			Please expect up to 3 business days for a reply.
+		</div>
+	</div>
+`;
+
+// "Jumbotron" heading element
+var $jumbotronHeading = $( '.jumbotron h1' )
+
+// This form currently is embedded in the portal homepage
+var $presalesForm = $( '#presales-form' )
+
+// Test if the current user appears to be logged in
+var isLoggedIn = $( '#menu-item-user' ).length === 1
+
+// Reverse of the above, to help us write cleaner conditionals
+var isLoggedOut = ! isLoggedIn
+
+// We'll use this for further comparisons
+var currentUrl = getCurrentUrl()
+
+// Test if we're on the support portal homepage
+var isHomePage = currentUrl.match(/\//) === null
+
+// Test if we're on the login page
+var isLoginPage = currentUrl.match( /login/ ) !== null
+
+// Test if we're on the My Tickets page
+var isMyTicketsPage = currentUrl.match( /my_tickets/ ) !== null
+
+// Test if we're on the Submit Ticket page
+var isSubmitTicketPage = currentUrl.match( /submit_ticket/ ) !== null
+
+var $marginTop = $( '.container.inner-margin-top' )
+var $navbar = $( '.navbar' )
+
+/**
+ * Return the page URL, but with any trailing slashes and the protocol 
+ * ("http://" or "https://") stripped for safer comparisons.
+ */
+function getCurrentUrl() {
+	var url = document.location.toString()
+	var protocolSeparator = url.match( /:\/\// )
+
+	// Remove trailing slash for uniformity
+	if ( '/' === url.slice( -1 ) ) {
+		url = url.slice( 0, -1 )
+	}
+
+	// Remove the protocol
+	if ( protocolSeparator !== null ) {
+		url = url.slice( protocolSeparator.index + 3 )
+	}
+
+	return url;
+}
+
+function addMarketingAlert( message ) {
+	// Add the tribe-marketing-alert class to the body tag so we can style appropriately
+	document.body.setAttribute('class', document.body.getAttribute('class') + ' tribe-marketing-alert')
+	// Add the actual message
+	$navbar.before( message )
+}
+
+function addJumbotronMessage( message ) {
+	// Add the tribe-jumbotron-message class to the body tag so we can style appropriately
+	document.body.setAttribute('class', document.body.getAttribute('class') + ' tribe-jumbotron-message')
+
+	// Add the actual message
+	$jumbotronHeading.after( message )
+}
+
+function optionallyRemovePresalesForm() {
+	// If the user is not logged in or else if the presales form can't be located
+	// we should not interfere
+	if ( ! isLoggedIn || $presalesForm.length !== 1 ) {
+		return
+	}
+
+	$presalesForm.hide()
+}
+
+addMarketingAlert( marketingAlert )
+optionallyRemovePresalesForm()
---
+++ js/includes/knowledgebase.js
@@ -0,0 +1,9 @@
+/**
+ * Finds and removes any category boxes that are devoid of articles.
+ * 
+ * Unsure why LiveAgent even renders these, but it's not particularly
+ * useful so we'll default to getting rid of them.
+ */
+$( '.category-list' ).find( '.alert.alert-empty' ).each( function() {
+	$( this ).parents( '.col-md-4.col-sm-6' ).hide();
+} );
