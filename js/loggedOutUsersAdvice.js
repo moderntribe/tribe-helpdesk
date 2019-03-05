f8e4bf176129a31b33bfa25dec4167713486b106
barryhughes <barry@codingkills.me>
2019-03-04 16:35:17 -0800

    Remove a further duplicate definition | #123575

--- js/loggedOutUsersAdvice.js
+++
@@ -1,9 +0,0 @@
-// Some general advice we'll show as needed in the jumbotron area
-var loggedOutUsersAdvice = 
-'<p class="tribe-support-advice"> <strong>Existing customer? To receive the fastest support possible, '
-+ 'please <a href="https://support.theeventscalendar.com/login">login</a> before posting.</strong> '
-+ 'In some cases, you may need to register first of all: be sure to do so using the same email address '
-+ 'you used when purchasing your license key!</p>'
-+ '<p class="tribe-support-advice">To learn more about accessing support or if you experience any problems, '
-+ 'please refer to '
-+ '<a href="https://support.theeventscalendar.com/812859-Accessing-Support">this article</a>.</p>'
