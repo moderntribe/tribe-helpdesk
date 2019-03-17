24fb08985d59e731c3d45d022b60c13895a1b5ed
barryhughes <barry@codingkills.me>
2019-03-17 14:50:11 -0500

    Compile (new alert) | spotfix

--- js/includes/alert.js
+++ js/includes/alert.js
@@ -3,12 +3,12 @@
  * 
  * The following shows the expected structure (as supported by our current CSS):
  * 
- *	<div id="tribe-marketing-alert">
-			<div class="tribe-marketing-alert__content">
-				<p><span class="emoji">⚠️ Heads up!</span>
-				Response times for support requests are a little longer than usual. Please expect up to 3 business days for a reply.</p>
-			</div>
-		</div>
+ * 	<div id="tribe-marketing-alert">
+ * 		<div class="tribe-marketing-alert__content">
+ * 			<p><span class="emoji">⚠️ Heads up!</span>
+ * 			Response times for support requests are a little longer than usual. Please expect up to 3 business days for a reply.</p>
+ * 		</div>
+ * 	</div>
  */
 function addAlert( message ) {
 	// Add the tribe-marketing-alert class to the body tag so we can style appropriately
@@ -22,9 +22,9 @@ addAlert( `
 	<div id="tribe-marketing-alert">
 		<div class="tribe-marketing-alert__content">
 			<p><span class="emoji">⚠️ Heads up!</span>
-			Please note, the whole team is out this week for our annual team trip where we discuss how we can provide better products, services, and support for our clients. 
-			Due to this our response times in the Help Desk will be slower than usual. 
-			Thanks for your kind patience in advance during this time!</p>
+			Please note, the whole team is out this week for our annual team trip where we discuss how we can provide 
+			better products, services, and support for our clients. Due to this our response times in the Help Desk 
+			will be slower than usual. Thanks for your kind patience in advance during this time!</p>
 		</div>
 	</div>
 ` );
