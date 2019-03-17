ad462c43afb9635e7dfaba8bd1200e713275d5ce
Andras Guseo <andras@tri.be>
2019-03-17 12:46:27 -0500

    Update alert for team trip

--- js/includes/alert.js
+++ js/includes/alert.js
@@ -16,3 +16,15 @@ function addAlert( message ) {
 	// Add the actual message
 	$navbar.before( message )
 }
+
+/* Remove this when the time comes */
+addAlert( `
+	<div id="tribe-marketing-alert">
+		<div class="tribe-marketing-alert__content">
+			<p><span class="emoji">⚠️ Heads up!</span>
+			Please note, the whole team is out this week for our annual team trip where we discuss how we can provide better products, services, and support for our clients. 
+			Due to this our response times in the Help Desk will be slower than usual. 
+			Thanks for your kind patience in advance during this time!</p>
+		</div>
+	</div>
+` );
