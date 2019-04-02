fb303b470bb870981febe111f91bd956f24388b2
Andras Guseo <andras@tri.be>
2019-04-02 21:53:29 +0200

    Removing alert from support lander

--- js/includes/alert.js
+++ js/includes/alert.js
@@ -3,12 +3,14 @@
  * 
  * The following shows the expected structure (as supported by our current CSS):
  * 
+ * addAlert( `
  * 	<div id="tribe-marketing-alert">
  * 		<div class="tribe-marketing-alert__content">
  * 			<p><span class="emoji">⚠️ Heads up!</span>
  * 			Response times for support requests are a little longer than usual. Please expect up to 3 business days for a reply.</p>
  * 		</div>
  * 	</div>
+ * ` );
  */
 function addAlert( message ) {
 	// Add the tribe-marketing-alert class to the body tag so we can style appropriately
@@ -16,16 +18,3 @@ function addAlert( message ) {
 	// Add the actual message
 	$navbar.before( message )
 }
-
-/* Remove this when the time comes */
-addAlert( `
-	<div id="tribe-marketing-alert">
-		<div class="tribe-marketing-alert__content">
-			<p>
-				<span class="emoji">⚠️ Heads up!</span>
-				Our response times are slightly slower than usual. 
-				Please bear with us—we're working hard to catch up.
-			</p>
-		</div>
-	</div>
-` );
