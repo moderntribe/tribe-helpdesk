b6a5f2b1dd9c790c934954b341e8afda070e068c
geoffgraham <geoff@geoffgraham.me>
2019-04-05 12:40:04 -0700

    Article audit and cleanup.
    Signed-off-by: geoffgraham <geoff@geoffgraham.me>

--- js/includes/homepage.js
+++ js/includes/homepage.js
@@ -19,54 +19,54 @@ var jumbotronSearch = `
  * that analytics have shown are common customer inquiries.
 */
 var componentFeaturedContent = `
-<section class="featured-content">
-	<div class="featured-content__wrapper">
-		<a href="https://support.theeventscalendar.com/153124-Themers-Guide">
-			<article class="featured-content__item">
-				<div class="featured-content__icon">
-					<img src="http://theeventscalendar.com/content/uploads/2019/03/icon-themer.svg" alt="" />
-				</div>
-				<div class="featured-content__body">
-					<h2>Themer\'s Guide</h2>
-					<p>Every calendar view is a template that can be overridden in your theme.</p>
-				</div>
-			</article>
-		</a>
-		<a href="https://theeventscalendar.com/extensions/">
-			<article class="featured-content__item">
-				<div class="featured-content__icon">
-					<img src="http://theeventscalendar.com/content/uploads/2019/03/icon-add-window-2.svg" alt="" />
-				</div>
-				<div class="featured-content__body">
-					<h2>Extension Library</h2>
-					<p>Check out free mini-plugins to add additional features and settings to our plugins.</p>
-				</div>
-			</article>
-		</a>
-		<a href="https://theeventscalendar.com/functions/">
-			<article class="featured-content__item">
-				<div class="featured-content__icon">
-					<img src="http://theeventscalendar.com/content/uploads/2019/03/icon-code.svg" alt="" />
-				</div>
-				<div class="featured-content__body">
-					<h2>Plugin Functions</h2>
-					<p>Every available function in our products to use as filters for custom functionality.</p>
-				</div>
-			</article>
-		</a>
-		<a href="https://support.theeventscalendar.com/527363-Refund-policy">
-			<article class="featured-content__item">
-				<div class="featured-content__icon">
-					<img src="http://theeventscalendar.com/content/uploads/2019/03/icon-cart.svg" alt="" />
-				</div>
-				<div class="featured-content__body">
-					<h2>Orders & Refunds</h2>
-					<p>Is the plugin not right for your project? Here\'s info on orders and refund requests.</p>
-				</div>
-			</article>
-		</a>
-	</div>
-</section>
+	<section class="featured-content">
+		<div class="featured-content__wrapper">
+			<a href="https://support.theeventscalendar.com/153124-Themers-Guide">
+				<article class="featured-content__item">
+					<div class="featured-content__icon">
+						<img src="http://theeventscalendar.com/content/uploads/2019/03/icon-themer.svg" alt="" />
+					</div>
+					<div class="featured-content__body">
+						<h2>Themer\'s Guide</h2>
+						<p>Every calendar view is a template that can be overridden in your theme.</p>
+					</div>
+				</article>
+			</a>
+			<a href="https://theeventscalendar.com/extensions/">
+				<article class="featured-content__item">
+					<div class="featured-content__icon">
+						<img src="http://theeventscalendar.com/content/uploads/2019/03/icon-add-window-2.svg" alt="" />
+					</div>
+					<div class="featured-content__body">
+						<h2>Extension Library</h2>
+						<p>Check out free mini-plugins to add additional features and settings to our plugins.</p>
+					</div>
+				</article>
+			</a>
+			<a href="https://theeventscalendar.com/functions/">
+				<article class="featured-content__item">
+					<div class="featured-content__icon">
+						<img src="http://theeventscalendar.com/content/uploads/2019/03/icon-code.svg" alt="" />
+					</div>
+					<div class="featured-content__body">
+						<h2>Plugin Functions</h2>
+						<p>Every available function in our products to use as filters for custom functionality.</p>
+					</div>
+				</article>
+			</a>
+			<a href="https://support.theeventscalendar.com/527363-Refund-policy">
+				<article class="featured-content__item">
+					<div class="featured-content__icon">
+						<img src="http://theeventscalendar.com/content/uploads/2019/03/icon-cart.svg" alt="" />
+					</div>
+					<div class="featured-content__body">
+						<h2>Orders & Refunds</h2>
+						<p>Is the plugin not right for your project? Here\'s info on orders and refund requests.</p>
+					</div>
+				</article>
+			</a>
+		</div>
+	</section>
 `;
 
 /* PORTALS
@@ -74,61 +74,61 @@ var componentFeaturedContent = `
  * general topic, including a heading, intro and unordered list.
 */
 var componentPortals = `
-<div class="portals">
-	<div class="portals__portal">
-		<div class="portals__icon">
-			<img src="http://theeventscalendar.com/content/uploads/2019/04/helpdesk-icon-guides.svg" />
-		</div>
-		<div class="portals__content">
-			<h2>Getting Started</h2>
-			<p>Articles and tutorials on everything from getting started to customizing the plugins, troubleshooting, integrations and more!</p>
-			<ul>
-				<li><a href="https://support.theeventscalendar.com/342672-New-User-Primer-The-Events-Calendar-and-Events-Calendar-PRO">The Events Calendar</a></li>
-				<li><a href="https://support.theeventscalendar.com/259544-New-User-Primer-Event-Tickets--Event-Tickets-Plus">Event Tickets</a></li>
-				<li><a href="https://support.theeventscalendar.com/710770-New-User-Primer-Event-Aggregator">Event Aggregator</a></li>
-				<li><a href="https://support.theeventscalendar.com/890921-New-User-Primer-Community-Events">Community Events</a></li>
-			</ul>
-			<a href="https://theeventscalendar.com/knowledgebase-category/primers/" class="button button--primary">All Guides</a>
-		</div>
-		</div>
-	<div class="portals__portal">
-		<div class="portals__icon">
-			<img src="http://theeventscalendar.com/content/uploads/2019/04/helpdesk-icon-trending.svg" />
-		</div>
-		<div class="portals__content">
-			<h2>Trending Topics</h2>
-			<p>A few of the most popular guides folks are reading that you may also find helpful for getting the most from the plugins.</p>
-			<ul>
-				<li><a href="https://support.theeventscalendar.com/934573-Inserting-Calendar-Content-into-Posts-or-Pages">Shortcodes</a></li>
-				<li><a href="https://support.theeventscalendar.com/303643-Testing-for-conflicts">Testing for Conflicts</a></li>
-				<li><a href="https://theeventscalendar.com/customizations/">Customization</a></li>
-				<li><a href="https://support.theeventscalendar.com/969953-CSV-file-examples-for-importing">Importing Events</a></li>
-			</ul>
-			<a href="https://theeventscalendar.com/functions/" class="button button--primary">Visit Docs</a>
+	<div class="portals">
+		<div class="portals__portal">
+			<div class="portals__icon">
+				<img src="http://theeventscalendar.com/content/uploads/2019/04/helpdesk-icon-guides.svg" />
+			</div>
+			<div class="portals__content">
+				<h2>Getting Started</h2>
+				<p>Articles and tutorials on everything from getting started to customizing the plugins, troubleshooting, integrations and more!</p>
+				<ul>
+					<li><a href="https://support.theeventscalendar.com/342672-New-User-Primer-The-Events-Calendar-and-Events-Calendar-PRO">The Events Calendar</a></li>
+					<li><a href="https://support.theeventscalendar.com/259544-New-User-Primer-Event-Tickets--Event-Tickets-Plus">Event Tickets</a></li>
+					<li><a href="https://support.theeventscalendar.com/710770-New-User-Primer-Event-Aggregator">Event Aggregator</a></li>
+					<li><a href="https://support.theeventscalendar.com/890921-New-User-Primer-Community-Events">Community Events</a></li>
+				</ul>
+				<a href="https://theeventscalendar.com/knowledgebase-category/primers/" class="button button--primary">All Guides</a>
+			</div>
+			</div>
+		<div class="portals__portal">
+			<div class="portals__icon">
+				<img src="http://theeventscalendar.com/content/uploads/2019/04/helpdesk-icon-trending.svg" />
+			</div>
+			<div class="portals__content">
+				<h2>Trending Topics</h2>
+				<p>A few of the most popular guides folks are reading that you may also find helpful for getting the most from the plugins.</p>
+				<ul>
+					<li><a href="https://support.theeventscalendar.com/934573-Inserting-Calendar-Content-into-Posts-or-Pages">Shortcodes</a></li>
+					<li><a href="https://support.theeventscalendar.com/303643-Testing-for-conflicts">Testing for Conflicts</a></li>
+					<li><a href="https://theeventscalendar.com/customizations/">Customization</a></li>
+					<li><a href="https://support.theeventscalendar.com/969953-CSV-file-examples-for-importing">Importing Events</a></li>
+				</ul>
+				<a href="https://theeventscalendar.com/functions/" class="button button--primary">Visit Docs</a>
+			</div>
 		</div>
 	</div>
-</div>
 `;
 
 /* INTERSTITIAL
  * A component meant to spotlight a call-to-action.
 */
 var componentInterstitial = `
-<div class="interstitial">
-	<div class="interstitial__body interstitial--primary">
-		<div class="interstitial__icon">
-			<img src="http://theeventscalendar.com/content/uploads/2019/04/helpdesk-icon-support-help.svg">
-		</div>
-		<div class="interstitial__content">
-			<h2>Looking for More Help?</h2>
-			<p>If you have looked through the documentation and knowledgebase and still have questions, then reach out to our friendly support staff and we will help you out.</p>
-			<div class="interstitial__actions">
-				<a href="https://support.theeventscalendar.com/submit_ticket" class="button button--primary">Open Ticket</a>
-				<a href="https://wordpress.org/support/plugin/the-events-calendar/" class="interstitial__text-link">Free plugin support</a>
+	<div class="interstitial">
+		<div class="interstitial__body interstitial--primary">
+			<div class="interstitial__icon">
+				<img src="http://theeventscalendar.com/content/uploads/2019/04/helpdesk-icon-support-help.svg">
+			</div>
+			<div class="interstitial__content">
+				<h2>Looking for More Help?</h2>
+				<p>If you have looked through the documentation and knowledgebase and still have questions, then reach out to our friendly support staff and we will help you out.</p>
+				<div class="interstitial__actions">
+					<a href="https://support.theeventscalendar.com/submit_ticket" class="button button--primary">Open Ticket</a>
+					<a href="https://wordpress.org/support/plugin/the-events-calendar/" class="interstitial__text-link">Free plugin support</a>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
 `;
 
 /* CATEGORY LIST HEADING
