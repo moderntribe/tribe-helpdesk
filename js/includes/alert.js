6926ebfa8d062b1b52c06d488690202140adc521
Barry Hughes <barry@codingkills.me>
2019-11-28 19:00:51 -0330

    Add Black Friday 2019 / Thanksgiving banner | spotfix

--- js/includes/alert.js
+++ js/includes/alert.js
@@ -2,17 +2,29 @@
  * Adds an alert to the portal header.
  * 
  * The following shows the expected structure (as supported by our current CSS):
+ * 
  * <div id="tribe-marketing-alert">
- *		<div class="tribe-marketing-alert__content">
- *			<p><span class="emoji">⚠️ Heads up!</span>
- *			Response times for support requests are a little longer than usual. Please expect up to 3 business days for a reply.</p>
- *		</div>
- *	</div>
+ *     <div class="tribe-marketing-alert__content">
+ *         <p>
+ *             <span class="emoji">⚠️ Heads up!</span>
+ *			   Response times for support requests are a little longer than usual. Please expect up to 3 business days for a reply.
+ *         </p>
+ *     </div>
+ * </div>
  */
-
 function addAlert( message ) {
 	// Add the tribe-marketing-alert class to the body tag so we can style appropriately
 	document.body.setAttribute( 'class', document.body.getAttribute( 'class' ) + ' tribe-marketing-alert' )
 	// Add the actual message
-	$navbar.before( message )
+	$navbar.after( message )
 }
+
+addAlert( `
+	<div id="tribe-marketing-alert">
+		<div class="tribe-marketing-alert__content">
+			<p>
+				Please note that due to Thanksgiving Holidays in the US our response time might be a bit longer than usual.
+			</p>
+		</div>
+	</div>
+` );
