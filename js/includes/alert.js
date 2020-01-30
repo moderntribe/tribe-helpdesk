b8deed56f8ce5704b21726c9d7c87111d1716917
Andras Guseo <andras@tri.be>
2020-01-30 21:34:19 +0100

    hotfix message
    I hope I got the code right...

--- js/includes/alert.js
+++ js/includes/alert.js
@@ -14,7 +14,19 @@
  */
 function addAlert( message ) {
 	// Add the tribe-marketing-alert class to the body tag so we can style appropriately
-	document.body.setAttribute( 'class', document.body.getAttribute( 'class' ) + ' tribe-marketing-alert' )
+	document.body.setAttribute( 'class', document.body.getAttribute( 'class' ) + ' tribe-alert' )
 	// Add the actual message
 	$navbar.after( message )
 }
+
+/* Remove this when the time comes */
+addAlert( `
+	<div id="tribe-alert">
+		<div class="tribe-alert__content">
+			<p>
+				<span class="emoji">⚠️ </span>
+				We are working on a hotfix to remedy the issues with some events not displaying correctly with the new news. Expected release: January 31st, 2020. Workaround and more details can be found <a href="https://theeventscalendar.com/known-issues">here</a>. Thanks for your patience!
+			</p>
+		</div>
+	</div>
+` );
