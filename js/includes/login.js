09b98a5adee3a600be19979ac6b70e3b2a76e744
Barry Hughes <barry@codingkills.me>
2020-03-25 14:10:49 -0700

    Replace login field label with CSS to avoid conflicts with LA's own JS | CTOH-39

--- js/includes/login.js
+++
@@ -1,5 +0,0 @@
-// Test to see if a logged out user is trying to login.
-if ( isLoginPage && isLoggedOut ) {
-	// Replace the username field label to make it clear that this must be an email address.
-	$( '#login_usernamelabel' ).html( 'Your email address' );
-}
