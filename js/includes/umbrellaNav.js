93854d0fe77ee4197707c52328110c39e372a0f6
Bryan Farris <farris.bryan@gmail.com>
2020-04-14 16:04:59 -0500

    Add: URLs for Umbrella Nav

--- js/includes/umbrellaNav.js
+++ js/includes/umbrellaNav.js
@@ -5,13 +5,16 @@
 var umbrellaNav = `
 	<nav class="umbrella-nav">
 		<div class="container">
-			<ul class="umbrella-nav__list">
-				<li class="umbrella-nav__item"><a class="umbrella-nav__link" href="#">Home</a></li>
-				<li class="umbrella-nav__item"><a class="umbrella-nav__link" href="#">Knowledgebase</a></li>
-				<li class="umbrella-nav__item"><a class="umbrella-nav__link" href="#">DevDocs</a></li>
-				<li class="umbrella-nav__item"><a class="umbrella-nav__link" href="#">Translations</a></li>
-				<li class="umbrella-nav__item"><a class="umbrella-nav__link is-current-page" href="#">Support</a></li>
-				<li class="umbrella-nav__item"><a class="umbrella-nav__link" href="#">Demo</a></li>
+			<ul 
+				class="umbrella-nav__list"
+				aria-label="Umbrella Nav"
+			>
+				<li class="umbrella-nav__item"><a class="umbrella-nav__link" href="http://theeeventscalendar.com">Home</a></li>
+				<li class="umbrella-nav__item"><a class="umbrella-nav__link" href="https://theeventscalendar.com/knowledgebase/">Knowledgebase</a></li>
+				<li class="umbrella-nav__item"><a class="umbrella-nav__link" href="https://docs.theeventscalendar.com/">DevDocs</a></li>
+				<li class="umbrella-nav__item"><a class="umbrella-nav__link" href="https://translations.theeventscalendar.com/projects/">Translations</a></li>
+				<li class="umbrella-nav__item"><a class="umbrella-nav__link is-current-page" href="http://support.theeventscalendar.com/">Support</a></li>
+				<li class="umbrella-nav__item"><a class="umbrella-nav__link" href="https://demo.theeventscalendar.com/">Demo</a></li>
 			</ul>
 		</div>
 	</nav>
