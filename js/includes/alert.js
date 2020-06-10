7f2a3cb4335d4c4f2ba5e045b1d9ffefeca6dafa
barryhughes <barry@codingkills.me>
2020-06-10 08:24:13 -0700

    Add support load alert | CTPW-39

--- js/includes/alert.js
+++ js/includes/alert.js
@@ -18,3 +18,22 @@ function addAlert( message ) {
 	// Add the actual message
 	$navbar.after( message )
 }
+
+// 2020-06-15 09:00:00 (Pacific) as a millisec-based timestamp.
+const takeDownTime = 1592236800000;
+
+// Support notice regarding the Day of Action that took place on 2020-06-08.
+// Take down automatically when the takeDownTime is reached.
+if ( ( takeDownTime - Date.now() ) > 0 ) {
+	addAlert( `
+		<div id="tribe-marketing-alert">
+			<div class="tribe-marketing-alert__content">
+				<p>
+					<span class="emoji">⚠️ Heads up!</span>
+					Support responses will be delayed while we are trying to catch up after being closed for a Day of
+					Action against racial injustice on Monday, June 8. Thanks for your kind patience and understanding.
+				</p>
+			</div>
+		</div>
+	` );
+}
